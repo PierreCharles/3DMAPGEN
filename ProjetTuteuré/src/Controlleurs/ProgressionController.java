3e1e89ca5abecb85259e48f451e1274b641e3361
Thomas <thomas.klein@etu.udamail.fr>
2017-02-27 09:16:36 +0100

    ajout de fichiers oubliés

---
+++ ProjetTuteuré/src/Controlleurs/ProgressionController.java
@@ -0,0 +1,35 @@
+package Controlleurs;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Thomas Klein
+ */
+public class ProgressionController implements Initializable {
+   
+    private Stage thisStage;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    public void setStage(Stage thisStage) {
+        this.thisStage = thisStage;
+    } 
+    
+}
